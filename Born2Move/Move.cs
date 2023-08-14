07c05709bf8040d65cf24e442b2ddadb7004256e
Daan Braas <daanbraas@gmail.com>
2023-08-14 13:03:46 +0200

    Born2Move start

---
+++ Born2Move/Move.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Move
+{
+	private int id { get; set; }
+	private string name { get; set; }
+	private string description { get; set; }
+	private int sweatRate { get; set; }
+
+	public Move()
+	{
+	}
+}
---
+++ Born2Move/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Born2Move
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            try
+            {
+                Console.WriteLine("Hallo, Welkom!");
+                Console.WriteLine
+
+            }
+            finally
+            {
+                Console.WriteLine("Press enter to close...");
+                Console.ReadLine();
+            }
+#endif
+        }
+    }
+}
